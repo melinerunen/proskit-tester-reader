b09b4115d11bef86258ccfe4748a170e25b2f722
david.cortelezzi <melinerunen@gmail.com>
2017-03-30 15:08:31 -0300

    [MOD] Some changes to the model

--- proskit-reader.js
+++ proskit-reader.js
@@ -1,15 +1,30 @@
 /**
  * Created by david on 3/29/17.
  */
-const SerialPort = require("serialport-win-fix");
+const SerialPort = require("serialport");
 
-let model = {
-    "value": {"type": "string"}
+
+let data_model = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+
+    "rawValue": {"type": "integer"},
+    "value": {"type": "number"},
+    "units": {"type": "string"},
+
+    "precision": {"type": "integer"},
+
+    "autoRange": {"type": "boolean"},
+    "measureType": {"type": "string"},
+    //TODO: Add the rest of the model to d
 };
 
 let port = new SerialPort("/dev/cu.SLAB_USBtoUART", {
     baudRate: 2400,
-    parser: SerialPort.parsers.byteLength(14)
+    dataBits: 8,
+    stopBits: 1,
+    // bufferSize: 14,
+    // parser: SerialPort.parsers.raw,
+    parser: SerialPort.parsers.byteLength(14),
 });
 
 
@@ -22,13 +37,12 @@ port.on("open", function() {
 
 port.on("data", function(data) {
 
-    model.value = data.slice(0, 5).toString();
+    data_model.rawValue = data.slice(0, 5).toString('ascii');
 
-    console.log(model);
+    console.log(data_model);
     //TODO: Analize the rest of the segments
-    console.log(data.slice(6, 11));
-    console.log(data.slice(12, 14));
 
+    console.log(data.toString('ascii'));
 });
 
 port.on("close", function() {
