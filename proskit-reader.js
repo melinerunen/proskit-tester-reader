6c3602e7fce9779e191bcd18ff19b1a72f255c65
david.cortelezzi <melinerunen@gmail.com>
2017-03-30 02:26:34 -0300

    [ADD] First version of pharser

--- proskit-reader.js
+++ proskit-reader.js
@@ -1,7 +1,36 @@
 /**
  * Created by david on 3/29/17.
  */
-const serial = require(serialport);
+const SerialPort = require("serialport-win-fix");
+
+let model = {
+    "value": {"type": "string"}
+};
+
+let port = new SerialPort("/dev/cu.SLAB_USBtoUART", {
+    baudRate: 2400,
+    parser: SerialPort.parsers.byteLength(14)
+});
 
 
+port.on("open", function() {
+    console.log("Port is open");
+    console.log("Flushing data");
+    port.flush();
+});
 
+
+port.on("data", function(data) {
+
+    model.value = data.slice(0, 5).toString();
+
+    console.log(model);
+    //TODO: Analize the rest of the segments
+    console.log(data.slice(6, 11));
+    console.log(data.slice(12, 14));
+
+});
+
+port.on("close", function() {
+    console.log("port is closed!!!");
+});
